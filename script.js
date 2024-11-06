31e4f4d45eb957a692fa1e2f55e036ca45c48992
Mete Sertkan <55276021+MeteSertkan@users.noreply.github.com>
2024-11-06 19:40:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleList(id) {
+    const list = document.getElementById(id);
+    const triangle = list.previousElementSibling.previousElementSibling.querySelector(".triangle");
+
+    // Toggle display of the list
+    if (list.style.display === 'none' || list.style.display === '') {
+        list.style.display = 'block';
+        triangle.classList.add("open"); // Add rotation class
+    } else {
+        list.style.display = 'none';
+        triangle.classList.remove("open"); // Remove rotation class
+    }
+}
